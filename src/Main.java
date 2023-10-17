a1a4e6506fcced9bfba782d297b2d0ff8530a7c6
ESTUDIANTE <lopez.rikelmer@iemodeliaibague.edu.co>
2023-10-17 14:02:57 -0500

    Subida: Frist commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+         String []Nombres = {"Rikelmer","Riquelme","Marlon","Paola","Andrea","Sara","Mariana","Esmeralda","Sofia","Valentina"};
+
+                 for (String i:Nombres) {
+
+                             JOptionPane.showMessageDialog(null,"Nombres: \n"+i  );
+                 }
+    }
+
+}
+
+
+
